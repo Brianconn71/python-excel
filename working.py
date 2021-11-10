3d4ae598f8517ab8a93af3b7c1e9a918e23cf3ab
Brianconn71 <Brian.connolly71@gmail.com>
2021-11-10 21:53:06 +0000

    add: edited cell values

--- working.py
+++ working.py
@@ -1,3 +1,9 @@
 from openpyxl import Workbook, load_workbook
 
 wb = load_workbook('GTN.xlsx')
+ws = wb.active
+print(ws['A2'].value)
+ws['A2'].value = "Test"
+print(ws['A2'].value)
+
+wb.save('GTN.xlsx')
